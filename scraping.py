6316e86542bc9d155a5ef80210edc33f5c940385
Pedro <pedrocid@example.com>
2020-11-27 23:37:56 -0300

    first commit

---
+++ scraping.py
@@ -0,0 +1,18 @@
+import requests as rq
+from bs4 import BeautifulSoup as bs
+
+pagina = rq.get('https://www.pcfactory.cl/smartphones?categoria=432&papa=645')
+
+soup = bs(pagina.text, 'lxml')
+
+lista_producto = soup.find_all('div', class_='center-caluga')
+
+for elem in lista_producto:
+    marca = elem.find('span', class_='marca')
+    nombre = elem.find('span', class_='nombre')
+    precio = elem.find('span', class_='txt-precio')
+    if None in (marca, nombre, precio):
+        continue
+    print(marca.text.strip())
+    print(nombre.text.strip())
+    print(precio.text.strip(),"\n")
